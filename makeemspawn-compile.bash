518c5af7115f51756f8a5f15a7604b88732b33fc
Hybris95 <hybris_95@hotmail.com>
2013-04-18 12:00:58 -0400

    Version 1.0 Added the Jar creation in the scripts

--- makeemspawn-compile.bash
+++ makeemspawn-compile.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
 javac -Xlint:deprecation -cp "./class:./jars/craftbukkit.jar:./jars/Permissions.jar" -d "./class" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawn.java" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawnPlayerListener.java"
+cd ./class
+jar cvf "MakeEmSpawn.jar" ./plugin.yml ./com/
+mv MakeEmSpawn.jar ../jars/
--- makeemspawn-compile.bat
+++ makeemspawn-compile.bat
@@ -1,3 +1,6 @@
 @echo off
-@javac -Xlint:deprecation -cp "./class;./jars/craftbukkit.jar;./jars/Permissions.jar" -d "./class" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawn.java" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawnPlayerListener.java"
-@pause
+javac -Xlint:deprecation -cp "./class;./jars/craftbukkit.jar;./jars/Permissions.jar" -d "./class" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawn.java" "./src/com/hybris/bukkit/makeemspawn/MakeEmSpawnPlayerListener.java"
+cd ./class
+jar cvf "MakeEmSpawn.jar" ./plugin.yml ./com/
+move /Y MakeEmSpawn.jar ../jars/
+pause
